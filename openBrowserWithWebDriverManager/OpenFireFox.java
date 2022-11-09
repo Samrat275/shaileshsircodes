91fdada5c0cc9554a72d5944af15e2d44700e126
Samrat275 <samratshelke96@gmail.com>
2022-11-09 14:58:57 +0530

    for demo

---
+++ openBrowserWithWebDriverManager/OpenFireFox.java
@@ -0,0 +1,17 @@
+package openBrowserWithWebDriverManager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class OpenFireFox {
+
+	public static void main(String[] args) {
+WebDriverManager.firefoxdriver().setup();
+WebDriver driver= new FirefoxDriver();
+driver.get("https://www.flipkart.com");
+
+	}
+
+}
